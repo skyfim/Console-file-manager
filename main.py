9290ec43df6206ba4667c1057b09a323a2e6674c
Danzan's laptop <uchurovdazn@gmail.com>
2023-07-20 03:01:00 +0300

    Commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from modules import main_funcs as mf
-
+run = True
 
 task = input("""Select an action:
                     --create folder
                     --delete file/folder
                     --copy file/folder
-                    --view the contents of the working directory""")
+                    --view the contents of the working directory
+                    write there - """)
 
-if task == "create":
-    mf.create_new_folder()
+for i in range(2):
+    if task == "create":
+        mf.create_new_folder()
+    elif task == "delete":
+        mf.delete_folder()
+    elif task == "copy":
+        mf.copy_obj()
+    elif task == "exit":
+        break
